276e8e3cc3883efc5742071b17be0d2d79d82a8d
Nikesh <nikeshadhikari896@gmail.com>
2022-12-19 20:16:40 +0545

    timer on enter implemented need fixing

--- typo.js
+++ typo.js
@@ -27,6 +27,7 @@ window.addEventListener("keydown", (e)=>{
             genRandom();
         }
         displayNew();
+        count();
     }
 });
 
@@ -83,3 +84,45 @@ input.addEventListener("input", (e)=>{
     }
 
 });
+
+var mins=1;
+var secs=mins*20;
+
+function count() {
+    setTimeout('decrease()',60);
+}
+function decrease() {
+    if(document.getElementById) {
+        minutes=document.getElementById("minutes");
+        seconds=document.getElementById("seconds");
+        if(seconds<59) {
+            seconds.value=secs;
+        }
+        else {
+            minutes.textContent=getminutes();
+            seconds.textContent=getseconds();
+        }
+        if(mins<1) {
+            minutes.style.color="red";
+            seconds.style.color="red";
+        }
+        if(mins<0) {
+            alert('time up');
+            minutes.value=0;
+            seconds.value=0;
+        }
+        else {
+            secs--;
+            setTimeout('decrease()',1000);
+        }
+    }
+}
+     
+function getminutes() {
+    mins=Math.floor(secs/60);
+    return mins;
+}
+     
+function getseconds() {
+    return secs-Math.round(mins*60);
+}
